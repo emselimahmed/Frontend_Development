271d87dbf605f77cbd29b791f79dbadf907f1085
EM Selim Ahmed <33466187+emselimahmed@users.noreply.github.com>
2018-05-03 13:09:09 +0600

    Add files via upload

---
+++ 01. HTML/16 Breaking News Scrolling/News Head Line.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Breaking News</title>
+	<style>
+	marquee {
+		     font-size: 50px;
+	}
+	
+	</style>
+</head>
+<body>
+
+
+<marquee behavior="alternate" bgcolor="green">সামরিক উড়োজাহাজ বিধ্বস্ত হয়ে ৯ জনের মৃত্যুর আশঙ্কা...</marquee>
+<marquee direction="right" bgcolor="black" style="color:white;">ম্যুলারের মুখোমুখি হলে ট্রাম্পের ঝুঁকি বাড়তে পারে...</marquee>	
+<marquee behavior="slide" bgcolor="red">সীমান্তে আটকা যুক্তরাষ্ট্রে অভিবাসন-প্রত্যাশী শতাধিক মানুষ...</marquee>
+</body>
+</html>
