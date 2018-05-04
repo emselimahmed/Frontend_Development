c257d4aa9430ba1c4c2255ad30c6fbf3fac644f8
EM Selim Ahmed <33466187+emselimahmed@users.noreply.github.com>
2018-05-04 13:41:37 +0600

    Add files via upload

---
+++ 01. HTML/17 Video and Audio Player/Video and Audio Player 2 .php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Video and Audio Player</title>
+</head>
+<body>
+
+<video src="https://bit.ly/2HR3jnQ" type="video/mp4" width="560" height="360" controls>Demo Video</video>
+<br>
+<br>
+<br>
+<audio src="https://bit.ly/2JR1mIR" type="audio/mpeg" controls>The Best Remix Ever</audio>
+</body>
+</html>
---
+++ 01. HTML/18 HTML 5 Various Discussion/HTML 5 Various Discussion.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+</head>
+<body style="text-align: center;">
+<h1><center>All Division of Bangladesh</center></h1>
+<hr>
+<span><b>Select Your Division:-</b></span>
+<select>
+<option value="">Barishal Division</option>
+<option value="">Chattogram Division</option>
+<option value="">Dhaka Division</option>
+<option value="">Khulna Division</option>
+<option value="">Mymensingh Division</option>
+<option value="">Rajshahi Division</option>
+<option value="">Rangpur Division</option>
+<option value="">Sylhet Division</option>
+
+</select>
+<br>
+<br>
+<br>
+
+<b>Select your District:-</b>
+
+<select>
+	<optgroup label="Khulna Division">
+		<option value="">Jessore</option>
+			<option value="">Khulna</option>
+			<option value="">Satkhira</option>
+		<option value="">Kushtia</option>
+	</optgroup>
+	
+    <optgroup label="Dhaka Division">
+		<option value="">Dhaka</option>
+			<option value="">Gazipur</option>
+			<option value="">Narsingdi</option>
+		<option value="">Faridpur</option>
+	</optgroup>
+	
+	<optgroup label="Rajshahi Division">
+		<option value="">Rajshahi</option>
+			<option value="">Natore</option>
+			<option value="">Bogra</option>
+		<option value="">Chapainawabganj</option>
+	</optgroup>
+	
+	<optgroup label="Barisal Division">
+		<option value="">Barisal</option>
+			<option value="">Bhola</option>
+			<option value="">Pirojpur</option>
+		<option value="">Patuakhali</option>
+	</optgroup>
+	
+	<optgroup label="Chittagong Division">
+		<option value="">Comilla</option>
+			<option value="">Noakhali</option>
+			<option value="">Chandpur</option>
+		<option value="">Feni</option>
+	</optgroup>
+	
+	<optgroup label="Mymensingh Division">
+		<option value="">Jamalpur</option>
+			<option value="">Netrokona</option>
+			<option value="">Sherpur</option>
+		<option value="">Mymensingh</option>
+	</optgroup>
+	
+	<optgroup label="Rangpur Division">
+		<option value="">Thakurgaon </option>
+			<option value="">Rangpur </option>
+			<option value="">Dinajpur </option>
+		<option value="">Kurigram </option>
+	</optgroup>
+	
+	<optgroup label="Sylhet Division">
+		<option value="">Habiganj</option>
+			<option value="">Moulvibazar</option>
+			<option value="">Sunamganj</option>
+		<option value="">Sylhet</option>
+	</optgroup>
+</select>
+<br>
+<br>
+
+<b>Progress Bar:-</b> &nbsp;&nbsp;&nbsp;
+<progress value="70" max="100"></progress><br><br>
+
+<details>
+<summary><b>Bangladesh</b></summary>
+	 <p>
+	    Bangladesh, to the east of India on the Bay of Bengal,<br>
+		is a South Asian country marked by lush greenery andbr<br>
+		many waterways. Its Padma (Ganges), Meghna and Jamuna<br>
+		rivers create fertile plains, and travel by boat is common.<br>
+		On the southern coast, the Sundarbans, an enormous mangrove<br>
+		forest shared with Eastern India, is home to the royal Bengal<br>
+		tiger.
+	 </p>
+</details>
+	
+</body>
+</html>
